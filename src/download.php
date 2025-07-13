a04426ae7ad262f96aec16ea099f33b453615df7
DAH5 <contact@dah5.me.uk>
2025-07-13 16:01:58 +0100

    Updated Download page to use remote content.

--- src/download.php
+++ src/download.php
@@ -2,45 +2,12 @@
 layout: src/_includes/layout.php
 staticphp_nav_item: download
 page_title: Download
+remote_content_url: https://raw.githubusercontent.com/DAH5/StaticPHP/refs/heads/master/Download.md
+remote_content_placeholder: {{ remote_content }}
 ---
 
 <section class="hero">
     <div class="container">
-        <div class="heading">Download</div>
-        <div class="text">StaticPHP is available for download from <a href="https://redirect.dah5.me.uk/github/staticphp" target="_blank">GitHub</a> and <a href="https://redirect.dah5.me.uk/gitlab/staticphp" target="_blank">GitLab</a>.</div>
-    </div>
-</section>
-
-<section class="downloads">
-    <div class="container">
-        <div class="download highlight">
-            <div class="title">Launcher (Recommended)</div>
-
-            <ul>
-                <li>Easy Build Configuration</li>
-                <li>Auto Updates to Latest StaticPHP Version</li>
-                <li>Redundant Access to StaticPHP</li>
-            </ul>
-
-            <div class="buttons">
-                <a href="https://redirect.dah5.me.uk/github/staticphp/StaticPHP-Launcher" class="primary" target="_blank">GitHub</a>
-                <a href="https://redirect.dah5.me.uk/gitlab/staticphp/StaticPHP-Launcher" target="_blank">GitLab</a>
-            </div>
-        </div>
-
-        <div class="download">
-            <div class="title">StaticPHP</div>
-
-            <ul>
-                <li>Completely Offline</li>
-                <li>Launch in the Commandline or Custom Script</li>
-                <li>Manually Update to Next Version of StaticPHP</li>
-            </ul>
-
-            <div class="buttons">
-                <a href="https://redirect.dah5.me.uk/github/staticphp/StaticPHP" class="primary" target="_blank">GitHub</a>
-                <a href="https://redirect.dah5.me.uk/gitlab/staticphp/StaticPHP" target="_blank">GitLab</a>
-            </div>
-        </div>
+        {{ remote_content }}
     </div>
 </section>
