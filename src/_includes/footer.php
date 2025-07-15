46f5e890a7c1c0fa96a48651adf5e806281c9c79
DAH5 <contact@dah5.me.uk>
2025-07-15 15:50:01 +0100

    Updated DAH5 URL to WebURL.net

--- src/_includes/footer.php
+++ src/_includes/footer.php
@@ -1,7 +1,7 @@
 
             <div class="footer">
                 <div class="container">
-                    <p>Copyright &copy; <a href="https://redirect.dah5.me.uk/dah5" target="_blank">DAH5</a>. Powered by StaticPHP.</p>
+                    <p>Copyright &copy; <a href="https://weburl.net/dah5" target="_blank">DAH5</a>. Powered by StaticPHP.</p>
                 </div>
             </div>
         </div>
